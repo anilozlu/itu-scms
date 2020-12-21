1f82e3ab4dfef51f412e8ebd1c533102af3d8293
anilozlu <anilozlu@gmail.com>
2020-12-21 14:42:54 +0300

    Changed usernames recommitting

--- server.py
+++ server.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template
-
+from flaskext.mysql import MySQL
 import views
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object("settings")
     app.add_url_rule("/", view_func=views.home_page)
-    app.add_url_rule("/movies", view_func=views.clubs_page)
+    app.add_url_rule("/clubs", view_func=views.clubs_page)
     return app
 
 if __name__ == "__main__":
