b02ca335063d5f76c65055ca7489a725a7028cf6
anilozlu <anilozlu@gmail.com>
2020-12-23 01:37:01 +0300

    Added register and login pages, register page functional. TODO: implement login.

--- server.py
+++ server.py
@@ -1,5 +1,13 @@
 from flask import Flask, render_template
+from flask_login import LoginManager
 import views
+from views import get_user
+
+lm = LoginManager()
+
+@lm.user_loader
+def load_user(user_id):
+    return get_user(user_id)
 
 def create_app():
     app = Flask(__name__)
@@ -7,6 +15,10 @@ def create_app():
     app.add_url_rule("/", view_func=views.home_page)
     app.add_url_rule("/clubs", view_func=views.clubs_page)
     app.add_url_rule("/create_club", view_func=views.create_club, methods=["GET", "POST"])
+    app.add_url_rule("/login", view_func=views.login_page, methods=["GET", "POST"])
+    app.add_url_rule("/register", view_func=views.register_page, methods=["GET", "POST"])
+    lm.init_app(app)
+    lm.login_view= "login_page"
     return app
 
 if __name__ == "__main__":
--- views.py
+++ views.py
@@ -1,4 +1,5 @@
 from flask import render_template, request, redirect, url_for
+from passlib.hash import pbkdf2_sha256 as hasher
 import mysql.connector
 mydb = mysql.connector.connect(
     host="localhost",
@@ -12,7 +13,7 @@ mycursor.execute("USE scms")
 mycursor.execute("""CREATE TABLE IF NOT EXISTS Students(
     user_id SERIAL,
     mail VARCHAR(50),
-    password VARCHAR(50),
+    password VARCHAR(150),
     full_name VARCHAR(50),
     PRIMARY KEY (user_id));""")
 mycursor.execute("""CREATE TABLE IF NOT EXISTS Clubs(
@@ -36,7 +37,27 @@ def home_page():
     return render_template("home.html")
 
 def clubs_page():
-    return render_template("clubs.html")
+    mycursor.execute("SELECT * FROM Clubs")
+    clubs = list(mycursor)
+    if clubs:
+        length = len(clubs)
+    else:
+        length = 0
+    return render_template("clubs.html", len = length, clubs = clubs)
+
+def login_page():
+    return render_template("login.html")
+
+def register_page():
+    if request.method == "GET":
+        return render_template("register.html")
+    else:
+        user_mail = request.form["mail"]
+        user_name = request.form["name"]
+        user_password = hasher.hash(request.form["password"])
+        mycursor.execute("INSERT INTO Students (mail, full_name, password) VALUES (\""+ user_mail + "\", \""+ user_name +"\", \""+ user_password +"\");")
+        mydb.commit()
+        return redirect(url_for("home_page"))
 
 def create_club():
     if request.method == "GET":
