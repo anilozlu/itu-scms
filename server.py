9df1063161252c9571d79706b5950aec47dfe509
anilozlu <anilozlu@gmail.com>
2020-12-22 21:49:55 +0300

    Connected with mysql, club creation functionalities added.

--- server.py
+++ server.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template
-import mysql.connector
 import views
 
 def create_app():
@@ -7,7 +6,7 @@ def create_app():
     app.config.from_object("settings")
     app.add_url_rule("/", view_func=views.home_page)
     app.add_url_rule("/clubs", view_func=views.clubs_page)
-    app.add_url_rule("/create_club", view_func=views.create_club)
+    app.add_url_rule("/create_club", view_func=views.create_club, methods=["GET", "POST"])
     return app
 
 if __name__ == "__main__":
--- views.py
+++ views.py
@@ -1,5 +1,37 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for
+import mysql.connector
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="anil",
+    password="deneme"
+)
+mycursor = mydb.cursor(buffered=True)
 
+mycursor.execute("CREATE DATABASE IF NOT EXISTS scms")
+mycursor.execute("USE scms")
+mycursor.execute("""CREATE TABLE IF NOT EXISTS Students(
+    user_id SERIAL,
+    mail VARCHAR(50),
+    password VARCHAR(50),
+    full_name VARCHAR(50),
+    PRIMARY KEY (user_id));""")
+mycursor.execute("""CREATE TABLE IF NOT EXISTS Clubs(
+    club_id SERIAL,
+    name VARCHAR(50),
+    description TEXT,
+    PRIMARY KEY (club_id));""")
+mycursor.execute("""CREATE TABLE IF NOT EXISTS Events(
+    event_id SERIAL,
+    description TEXT,
+    name VARCHAR(100),
+    PRIMARY KEY (event_id));""")
+#mycursor.execute("""CREATE TABLE IF NOT EXISTS Student_clubs(
+#    user_id SERIAL,
+#    club_id SERIAL,
+#    role VARCHAR(30),
+#    visible TINYINT,
+#    PRIMARY KEY (user_id, club_id),
+#    FOREIGN KEY (club_id) REFERENCES Clubs(club_id));""")
 def home_page():
     return render_template("home.html")
 
@@ -7,4 +39,12 @@ def clubs_page():
     return render_template("clubs.html")
 
 def create_club():
-    return render_template("create_club.html")
+    if request.method == "GET":
+        return render_template("create_club.html")
+    else:
+        club_name = request.form["name"]
+        club_description = request.form["description"]
+        mycursor.execute("INSERT INTO Clubs (name, description) VALUES (\""+club_name+"\", \""+club_description+"\");")
+        mycursor.execute("SELECT * FROM Clubs;")
+        mydb.commit()
+        return redirect(url_for("create_club"))
